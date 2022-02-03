eb0f6ee62e0308f99038488afd415f1c761fa48a
Nepo Rojas <braulio.rojas@islas.org.mx>
2022-02-02 22:08:02 -0800

    🚧 Add report with bokeh plot

---
+++ myapp/secod_try.py
@@ -0,0 +1,30 @@
+from bokeh.plotting import figure
+from bokeh.resources import CDN
+from bokeh.embed import file_html, components
+from bokeh.io import save
+import numpy as np
+from jinja2 import Environment, FileSystemLoader
+
+N = 4000
+x = np.random.random(size=N) * 100
+y = np.random.random(size=N) * 100
+radii = np.random.random(size=N) * 1.5
+colors = np.array([ [r, g, 150] for r, g in zip(50 + 2*x, 30 + 2*y) ], dtype="uint8")
+
+TOOLS="hover,crosshair,pan,wheel_zoom,zoom_in,zoom_out,box_zoom,undo,redo,reset,tap,save,box_select,poly_select,lasso_select,"
+
+p = figure(tools=TOOLS)
+
+p.scatter(x, y, radius=radii,
+          fill_color=colors, fill_alpha=0.6,
+          line_color=None)
+
+
+html = file_html(p, CDN, "my_plot.html")
+save(p, "my_plot.html")
+script, div = components(p)
+fileLoader = FileSystemLoader("myapp")
+env = Environment(loader=fileLoader)
+
+rendered = env.get_template("template.html").render(script = script, div=div)
+print(rendered)
