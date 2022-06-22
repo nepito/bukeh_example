2fefbbdb0eda873e4e7cb082ac95044c4b45304b
Nepo Rojas <braulio.rojas@islas.org.mx>
2022-06-21 21:54:05 -0700

    🚧 Make render

---
+++ src/render_metrics.py
@@ -0,0 +1,19 @@
+import pandas as pd
+from jinja2 import Environment, FileSystemLoader
+metrics = pd.read_csv("data/normalized_metrics.csv")
+
+def get_str_metrics(index):
+    mind_metrics = ["player_appearences", "goal_total", "goal_assists", "passes_key", "shots_on", "dribbles_success", "tackles_interceptions"]
+    metrics_of_player_1 = list(metrics.loc[index][mind_metrics])
+    str_player_1 = [str(metric) for metric in metrics_of_player_1]
+    return str.join(', ', str_player_1)
+
+element_player_1 = get_str_metrics(1)
+to_render = {"str_player_1": element_player_1}
+
+fileLoader = FileSystemLoader("reports")
+env = Environment(loader=fileLoader)
+
+all_players = {f"player_{player+1}":get_str_metrics(player) for player in range(4)}
+rendered = env.get_template("ejemplo_1.html").render(**all_players)
+print(rendered)
