4838239e2af544617f134f6a19dd5aa3ff5524d5
Nepo Rojas <braulio.rojas@islas.org.mx>
2022-06-27 17:48:28 -0700

    🚧 Add plot of goals and xG

--- src/render_metrics.py
+++ src/render_metrics.py
@@ -1,6 +1,50 @@
 import pandas as pd
 from jinja2 import Environment, FileSystemLoader
+from bokeh.io import show
+from bokeh.plotting import figure, ColumnDataSource
+from bokeh.embed import components
+from bokeh.colors import RGB
+
 metrics = pd.read_csv("data/normalized_metrics.csv")
+def get_player(path, name):
+    player = pd.read_csv(path)
+    selected_columns = ["Date","Goals","xG", "Match", "Minutes played"]
+    just_mx = player["Competition"] == "Mexico. Liga MX"
+    cleaned_player = player[just_mx][selected_columns]
+    cleaned_player["player"] = name
+    cleaned_player["Date"] = pd.to_datetime(cleaned_player["Date"])
+    return cleaned_player
+
+berterame = get_player("data/berterame_wyscout.csv", "Germán Berterame")
+aguirre = get_player("data/aguirre_wyscout.csv", "Rodrigo Aguirre")
+TOOLTIPS = [
+    ("Partido", "@{Match}"),
+    ("Minutos Jugados", "@{Minutes played}"),
+    ("xG", "@{xG}"),
+]
+data = ColumnDataSource(berterame)
+data2 = ColumnDataSource(aguirre)
+plot = figure(x_axis_type='datetime',
+              title='Goles de Berterame en la Liga MX',
+             x_axis_label = "Date",
+              y_axis_label = "Goles",
+	      tooltips=TOOLTIPS)
+plot.step(x='Date', y = 'Goals', source = data, color = 'blue')
+plot.line(x='Date', y = 'Goals', source = data, color = RGB(54, 162, 235, 0.0))
+show(plot)
+
+script, div = components(plot)
+
+plot_ra = figure(x_axis_type='datetime',
+              title='Goles de Aguirre en la Liga MX',
+             x_axis_label = "Fecha",
+              y_axis_label = "Goles",
+	      tooltips=TOOLTIPS)
+plot_ra.step(x='Date', y = 'Goals', source = data2, color = 'red')
+plot_ra.line(x='Date', y = 'Goals', source = data2, color = RGB(54, 162, 235, 0.0))
+show(plot_ra)
+
+script_ra, div_ra = components(plot_ra)
 
 def get_str_metrics(index):
     mind_metrics = ["player_minutes", "goal_total", "goal_assists", "passes_key", "shots_on", "dribbles_success", "tackles_interceptions"]
@@ -12,5 +56,5 @@ fileLoader = FileSystemLoader("reports")
 env = Environment(loader=fileLoader)
 
 all_players = {f"player_{player+1}":get_str_metrics(player) for player in range(4)}
-rendered = env.get_template("ejemplo_1.html").render(**all_players)
+rendered = env.get_template("ejemplo_1.html").render(script=script, div=div, script_ra=script_ra, div_ra=div_ra, **all_players)
 print(rendered)
