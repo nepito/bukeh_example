44c23ebd751c5ace7d589e3bc36bc8f64c4fa7ad
Nepo Rojas <braulio.rojas@islas.org.mx>
2022-07-03 14:18:18 -0700

    🎨 Format with black

--- src/example_intervals.py
+++ src/example_intervals.py
@@ -7,13 +7,24 @@ from bokeh.colors import RGB
 
 
 sprint.Year = sprint.Year.astype(str)
-group = sprint.groupby('Year')
+group = sprint.groupby("Year")
 source = ColumnDataSource(group)
 
-p = figure(y_range=group, x_range=(9.5,12.7), width=400, height=550, toolbar_location=None,
-           title="Time spreads for sprint medalists (by year)")
-p.hbar(y="Year", left='Time_min', right='Time_max', height=0.4, source=source)
-p.patch([10.5, 10.5, 11.5, 11.5], [0, len(group), len(group), 0], color=RGB(255, 99, 132, 0.2), line_width=0)
+p = figure(
+    y_range=group,
+    x_range=(9.5, 12.7),
+    width=400,
+    height=550,
+    toolbar_location=None,
+    title="Time spreads for sprint medalists (by year)",
+)
+p.hbar(y="Year", left="Time_min", right="Time_max", height=0.4, source=source)
+p.patch(
+    [10.5, 10.5, 11.5, 11.5],
+    [0, len(group), len(group), 0],
+    color=RGB(255, 99, 132, 0.2),
+    line_width=0,
+)
 
 p.ygrid.grid_line_color = None
 p.xaxis.axis_label = "Time (seconds)"
