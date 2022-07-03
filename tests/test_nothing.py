559d520e0dc5fda068bf85aa66ec40030ebe8a9c
Nepo Rojas <braulio.rojas@islas.org.mx>
2022-07-03 12:23:05 -0700

    🛑🧪 Test `src/example_bar_plots.py`

--- tests/test_nothing.py
+++ tests/test_nothing.py
@@ -1,3 +1,5 @@
+import hashlib
+import os
 import xg_plots as xgp
 
 
@@ -5,3 +7,10 @@ def test_return_one():
     expected = 1
     obtained = xgp.return_one()
     assert expected == obtained
+
+
+def test_example_bar_plots():
+    os.system('python src/example_bar_plots.py > salidita.html')
+    obtained_hash = hashlib.md5(open('salidita.html','rb').read()).hexdigest()
+    expected_hash = "931ff61ed21857e2121b99e85186051d"
+    assert expected_hash == obtained_hash
