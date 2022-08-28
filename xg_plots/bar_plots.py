6d869283596d5d2893578517475e42f11ca5fb0d
Nepo Rojas <braulio.rojas@islas.org.mx>
2022-08-28 16:56:03 -0700

    ✅ Pass test

--- xg_plots/bar_plots.py
+++ xg_plots/bar_plots.py
@@ -1,2 +1,7 @@
 def get_match(df_possiession, match):
-    pass
+    team = df_possiession.columns[3]
+    df_possiession = df_possiession.sort_values(by=[team])
+    primer_partido = list(df_possiession.match)[match]
+    primer_partido = primer_partido.split(" ")
+    primer_partido.pop().replace(":", " a ")
+    return " ".join(primer_partido).replace("-", "vs")
