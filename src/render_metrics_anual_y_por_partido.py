a45b3b0dc072409381f96e13d62a348a69acf68b
Nepo Rojas <braulio.rojas@islas.org.mx>
2022-08-29 20:08:52 -0700

    ♻️ Use function `render_all_report()`

--- src/render_metrics_anual_y_por_partido.py
+++ src/render_metrics_anual_y_por_partido.py
@@ -6,6 +6,8 @@ from bokeh.models import Tabs
 import pandas as pd
 from xg_plots import (
     get_match,
+    render_all_report,
+    get_info_to_write,
     Plotter_Intervals_From_Rivals,
     COLOR,
     COLOR_IN_TEXT,
@@ -13,28 +15,22 @@ from xg_plots import (
 
 
 df_possiession = pd.read_csv("data/output_morelia.csv")
-team = df_possiession.columns[3]
-primer_partido, marcador = get_match(df_possiession, match=-1)
-rival = list(df_possiession.rival_teams)[-1]
-schema_rival = list(df_possiession.scheme_rival)[-1]
-schema_team = list(df_possiession.scheme_team)[-1]
-ultimo_partido, ultimo_marcador = get_match(df_possiession, match=0)
-ultimo_rival = list(df_possiession.rival_teams)[0]
-menor_posesion = list(df_possiession[team])[0]
+info_to_write = get_info_to_write(df_possiession)
+team = info_to_write["team"]
 
 
 colors = COLOR["Morelia"]
-possession = [df_possiession.columns[3], "Rivales"]
+possession = [team, "Rivales"]
 TOOLTIPS = [
     ("Juego", "@{match}"),
     ("Sistema rival", "@{scheme_rival}"),
-    (f"Sistema {df_possiession.columns[3]}", "@{scheme_team}"),
+    (f"Sistema {team}", "@{scheme_team}"),
 ]
 sorted_equipos_rivales = df_possiession["rival_teams"]
 p = figure(
     y_range=sorted_equipos_rivales,
     height=350,
-    title=f"Posesión en los partidos de los {df_possiession.columns[3]}",
+    title=f"Posesión en los partidos de los {team}",
     toolbar_location=None,
     tools="hover",
     tooltips=TOOLTIPS,
@@ -51,7 +47,7 @@ p.outline_line_color = None
 p.legend.location = "top_left"
 p.legend.orientation = "horizontal"
 p.xaxis.axis_label = "Posesión (%)"
-p.yaxis.axis_label = f"{df_possiession.columns[3]} vs"
+p.yaxis.axis_label = f"{team} vs"
 
 script, div = components(p)
 
@@ -63,28 +59,15 @@ TOOLTIPS = [
 
 
 path = "data/metrics_intervals_morelia.csv"
-plotter = Plotter_Intervals_From_Rivals(path, team)
-tabls = [plotter.plot_intervals(x + 1, TOOLTIPS) for x in range(5)]
-p = Tabs(tabs=tabls)
-script_interval, div_interval = components(p)
-fileLoader = FileSystemLoader("reports")
-env = Environment(loader=fileLoader)
 
-rendered = env.get_template("metricas_anual_y_por_partido.html").render(
-    script=script,
-    div=div,
-    script_interval=script_interval,
-    div_interval=div_interval,
-    team=team,
-    primer_partido=primer_partido,
-    marcador=marcador,
-    schema_rival=schema_rival,
-    schema_team=schema_team,
-    rival=rival,
-    ultimo_partido=ultimo_partido,
-    ultimo_marcador=ultimo_marcador,
-    ultimo_rival=ultimo_rival,
-    menor_posesion=menor_posesion,
-    color=COLOR_IN_TEXT["Correcaminos"],
-)
-print(rendered)
+
+def plot_intrvals_of_the_last_five_matches(path, team, TOOLTIPS):
+    plotter = Plotter_Intervals_From_Rivals(path, team)
+    tabls = [plotter.plot_intervals(x + 1, TOOLTIPS) for x in range(5)]
+    p = Tabs(tabs=tabls)
+    return components(p)
+
+
+script_interval, div_interval = plot_intrvals_of_the_last_five_matches(path, team, TOOLTIPS)
+
+render_all_report(script, div, script_interval, div_interval, info_to_write, "Morelia")
