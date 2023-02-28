9dac511de199477732ba6e153cace5fa150ce353
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-02-28 07:15:38 -0800

    🚧 Add example of scatter plot

---
+++ src/scatter_example.py
@@ -0,0 +1,38 @@
+from jinja2 import Environment, FileSystemLoader
+
+
+from bokeh.embed import components
+
+from bokeh.models import Panel, Tabs
+from bokeh.plotting import figure
+
+from bokeh.plotting import figure
+from bokeh.models import ColumnDataSource, Grid, ImageURL
+
+url = "https://raw.githubusercontent.com/nepito/calculator-trs/develop/tests/data/logo_nies.png"
+# create a Python dict as the basis of your ColumnDataSource
+data = {'x_values': [1, 2, 3, 4, 5],
+        'y_values': [6, 7, 2, 3, 6]}
+
+# create a ColumnDataSource by passing the dict
+source = ColumnDataSource(data=data)
+
+# create a plot using the ColumnDataSource's two columns
+p = figure(title="Goles vs Asistencias")
+
+p.scatter(x='x_values', y='y_values', source=source)
+
+image3 = ImageURL(url=dict(value=url), x=0, y=0, h = 0.5, w=1, anchor="bottom_left")
+p.add_glyph(source, image3)
+p.xaxis.axis_label = 'Goles por cada 90 minutos'
+p.yaxis.axis_label = 'Asistencias por cada 90 minutos'
+
+script, div = components(p)
+fileLoader = FileSystemLoader("reports")
+env = Environment(loader=fileLoader)
+
+rendered = env.get_template("template.html").render(
+    script=script,
+    div=div,
+)
+print(rendered)
