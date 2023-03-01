029452324b5e9ef908537c4dfe85715547f72c1c
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-02-28 19:58:04 -0800

    🚧 Remove goal keepers

--- src/clean_data_for_scatter.R
+++ src/clean_data_for_scatter.R
@@ -3,4 +3,5 @@ library(tidyverse)
 players <- read_csv("data/LigaMX.csv", show_col_types = FALSE) |>
   janitor::clean_names() |>
   filter(minutes_played > 900) |>
+  filter(position != "GK") |>
   write_csv("data/player_for_scatter.csv")
