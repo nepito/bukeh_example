8a7dfdfbc40993c8da77516aee121af19bd5663d
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-02-28 18:48:23 -0800

    🎨 Format with Black

--- src/scatter_example.py
+++ src/scatter_example.py
@@ -24,8 +24,8 @@ p = figure(
 
 median = players.median()
 p.scatter(x="goals_per_90", y="assists_per_90", source=source)
-hline = Span(location=median["assists_per_90"], dimension='width', line_color='green', line_width=2)
-vline = Span(location=median["goals_per_90"], dimension='height', line_color='green', line_width=2)
+hline = Span(location=median["assists_per_90"], dimension="width", line_color="green", line_width=2)
+vline = Span(location=median["goals_per_90"], dimension="height", line_color="green", line_width=2)
 
 image3 = ImageURL(url=dict(value=url), x=0.6, y=0.34, h=0.03, w=0.15, anchor="bottom_left")
 p.add_glyph(source, image3)
