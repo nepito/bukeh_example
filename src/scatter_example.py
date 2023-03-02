c8c2dcbf273496c735bff1f35fcac755afc5b9a0
Nepo Rojas <braulio.rojas@islas.org.mx>
2023-03-01 19:11:59 -0800

    🎨 Format with Black

--- src/scatter_example.py
+++ src/scatter_example.py
@@ -24,11 +24,25 @@ p = figure(
 
 median = players.median()
 p.circle(x="goals_per_90", y="assists_per_90", radius="radio", source=source)
-hline = Span(location=median["assists_per_90"], dimension="width", line_color="green", line_width=2)
-vline = Span(location=median["goals_per_90"], dimension="height", line_color="green", line_width=2)
+hline = Span(
+    location=median["assists_per_90"],
+    dimension="width",
+    line_color="green",
+    line_width=2,
+    line_dash="dashed",
+)
+vline = Span(
+    location=median["goals_per_90"],
+    dimension="height",
+    line_color="green",
+    line_width=2,
+    line_dash="dashed",
+)
 
 height = 0.03
-image3 = ImageURL(url=dict(value=url), x=0.6, y=0.34, h=height, w=3.95*height, anchor="bottom_left")
+image3 = ImageURL(
+    url=dict(value=url), x=0.78, y=0.34, h=height, w=3.95 * height, anchor="bottom_left"
+)
 p.add_glyph(source, image3)
 p.xaxis.axis_label = "Goles por cada 90 minutos"
 p.yaxis.axis_label = "Asistencias por cada 90 minutos"
